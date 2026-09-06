5ba3a78af2f47709d32811877b81dd7f6ec8e84b
carjam120443-netizen <carjam120443@gmail.com>
2026-09-06 16:32:49 -0400

    Add CarsonCC x86-64 codegen stub

---
+++ src/codegen.c
@@ -0,0 +1,8 @@
+#include "codegen.h"
+
+int codegen_x86_64(void *ir, const char *output_path) {
+    (void)ir;
+    (void)output_path;
+    /* x86-64 instruction selection will be implemented after the IR. */
+    return 0;
+}
