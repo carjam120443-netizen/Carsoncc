77e0209d16b693a087256e72f6e8d5fa4d52e94d
carjam120443-netizen <carjam120443@gmail.com>
2026-09-06 16:37:47 -0400

    Define x86-64 code generation API

--- src/codegen.h
+++ src/codegen.h
@@ -1,7 +1,5 @@
 #ifndef CARSONCC_CODEGEN_H
 #define CARSONCC_CODEGEN_H
-
-/* Target code generation interface. The first backend will target x86-64. */
-int codegen_x86_64(void *ir, const char *output_path);
-
+#include "ast.h"
+int codegen_x86_64(const AstNode *program, const char *output_path);
 #endif
