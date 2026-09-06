531dd1b6a87d25c37893f24b8b2d7e25251071ce
carjam120443-netizen <carjam120443@gmail.com>
2026-09-06 16:48:09 -0400

    Expose native Windows x64 backend

--- src/codegen.h
+++ src/codegen.h
@@ -2,4 +2,5 @@
 #define CARSONCC_CODEGEN_H
 #include "ast.h"
 int codegen_x86_64(const AstNode *program, const char *output_path);
+int codegen_windows_x86_64(const AstNode *program, const char *output_path);
 #endif
