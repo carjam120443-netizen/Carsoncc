f3689dac56ce0dc26690739600cdbaaef53f7176
carjam120443-netizen <carjam120443@gmail.com>
2026-09-06 16:35:38 -0400

    Expand AST for real compiler pipeline

--- src/ast.h
+++ src/ast.h
@@ -1,20 +1,26 @@
 #ifndef CARSONCC_AST_H
 #define CARSONCC_AST_H
 
-/* Abstract Syntax Tree definitions will grow with the language grammar. */
-typedef enum {
-    AST_PROGRAM,
-    AST_FUNCTION,
-    AST_RETURN,
-    AST_INTEGER,
-    AST_BINARY
-} AstKind;
+#include <stddef.h>
 
-typedef struct AstNode {
+typedef enum { AST_PROGRAM, AST_FUNCTION, AST_BLOCK, AST_RETURN, AST_LET, AST_INTEGER, AST_BINARY } AstKind;
+typedef enum { OP_ADD, OP_SUB, OP_MUL, OP_DIV } BinaryOp;
+
+typedef struct AstNode AstNode;
+struct AstNode {
     AstKind kind;
-    struct AstNode *left;
-    struct AstNode *right;
+    size_t line;
+    char *name;
     long integer_value;
-} AstNode;
+    BinaryOp op;
+    AstNode *left;
+    AstNode *right;
+    AstNode **children;
+    size_t child_count;
+};
+
+AstNode *ast_new(AstKind kind, size_t line);
+void ast_free(AstNode *node);
+int ast_add_child(AstNode *parent, AstNode *child);
 
 #endif
